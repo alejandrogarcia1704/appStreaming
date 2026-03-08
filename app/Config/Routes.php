66a56666c2835179c8ae32399102e3c0c9a0d6eb
Bra941 <brandonjaviervillegas@gmail.com>
2026-03-08 07:55:01 -0600

    corrección

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,5 +1,43 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\Services;
+
+$routes = Services::routes();
+
+/*
+---------------------------------------------------------------
+Load System's Routing File First
+---------------------------------------------------------------
+*/
+if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
+    require SYSTEMPATH . 'Config/Routes.php';
+}
+
+/*
+---------------------------------------------------------------
+Router Setup
+---------------------------------------------------------------
+*/
+
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Auth');
+$routes->setDefaultMethod('loginView');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+$routes->setAutoRoute(true);
+
+/*
+---------------------------------------------------------------
+Route Definitions
+---------------------------------------------------------------
+*/
+
 $routes->get('/', 'Auth::loginView');
+
 $routes->post('/login', 'Auth::login');
+
 $routes->get('/register', 'Auth::registerView');
 $routes->post('/register', 'Auth::register');
 
@@ -13,3 +51,13 @@ $routes->post('/upload', 'Upload::uploadFile');
 
 $routes->get('/forgot', 'Auth::forgotView');
 $routes->post('/forgot', 'Auth::forgotPassword');
+
+/*
+---------------------------------------------------------------
+Additional Environment Routes
+---------------------------------------------------------------
+*/
+
+if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
+    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
+}
